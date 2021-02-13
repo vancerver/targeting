e495dc0598ba1ee6ea6763bf215c530285aea98d
Vance Cagle <vancecagle@Vances-MacBook-Pro-2.local>
2021-02-13 10:03:32 -0800

    Initial commit - Minimal setup from Spring Initializr

---
+++ src/main/java/com/vancerver/targeting/TargetingApplication.java
@@ -0,0 +1,13 @@
+package com.vancerver.targeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TargetingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TargetingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vancerver/targeting/TargetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vancerver.targeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TargetingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
