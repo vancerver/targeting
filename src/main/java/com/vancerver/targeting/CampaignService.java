8979dfa0531cca20291d60c4bc485404af70f5a1
Vance Cagle <vance@vancerver.com>
2021-02-14 15:18:08 -0800

    Adds empty CampaignService class

---
+++ src/main/java/com/vancerver/targeting/CampaignService.java
@@ -0,0 +1,7 @@
+package com.vancerver.targeting;
+@Service
+public class CampaignService {
+    // Add a basic logger that logs to the console
+    private static Logger LOG = LoggerFactory
+            .getLogger(CampaignService.class);
+}
