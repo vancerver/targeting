a8532729c43a025c54012e1fad95043b95fce5aa
Vance Cagle <vance@vancerver.com>
2021-02-14 15:21:08 -0800

    Modify the main class to use command line

--- src/main/java/com/vancerver/targeting/TargetingApplication.java
+++ src/main/java/com/vancerver/targeting/TargetingApplication.java
@@ -1,10 +1,11 @@
 package com.vancerver.targeting;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TargetingApplication {
+public class TargetingApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(TargetingApplication.class, args);
